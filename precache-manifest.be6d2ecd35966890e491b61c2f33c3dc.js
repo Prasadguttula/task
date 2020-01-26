209208089658c82f93ebaaacea48d098d7dafc73
Prasadguttula <prasadguttula68@gmail.com>
2020-01-27 00:17:36 +0530

    Updates

---
+++ precache-manifest.be6d2ecd35966890e491b61c2f33c3dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88332207ded3a53b619be322d9b87dd",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "247ffd6b95113ba3c382",
+    "url": "/task/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e76fffdba9dda377599f",
+    "url": "/task/static/css/main.3f79ea04.chunk.css"
+  },
+  {
+    "revision": "247ffd6b95113ba3c382",
+    "url": "/task/static/js/2.110bb28d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/task/static/js/2.110bb28d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e76fffdba9dda377599f",
+    "url": "/task/static/js/main.e98feecf.chunk.js"
+  },
+  {
+    "revision": "55d9ac6d7afd6f188f4d",
+    "url": "/task/static/js/runtime-main.c876d478.js"
+  }
+]);
